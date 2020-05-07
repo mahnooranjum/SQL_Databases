05d0cb0a3ffda5490800a6779aa48a0917571de4
Mahnoor Anjum <manomaq@gmail.com>
2020-05-07 07:21:07 +0500

    updated repository

---
+++ s2_where.sql
@@ -0,0 +1,8 @@
+-- SELECT * FROM customer WHERE first_name = 'Mary';
+-- SELECT * FROM customer WHERE first_name = 'Mary' OR first_name = 'Andy';
+-- SELECT * FROM payment WHERE amount >= 2 AND amount <= 3 ;
+-- SELECT DISTINCT staff_id FROM payment;
+-- SELECT * FROM customer;
+-- SELECT email FROM customer WHERE first_name = 'Jennifer' AND last_name = 'Davis';
+-- SELECT description FROM film WHERE title='Ace Goldfinger';
+SELECT phone FROM address WHERE address='1121 Loja Avenue';
