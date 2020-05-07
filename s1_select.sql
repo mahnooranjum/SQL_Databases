7b1cc3fc73989154a645b113a61bba97bccb0155
Mahnoor Anjum <manomaq@gmail.com>
2020-05-07 07:20:26 +0500

    added 1

---
+++ s1_select.sql
@@ -0,0 +1,14 @@
+-- SELECT * FROM film;
+
+-- SELECT first_name, last_name, email 
+-- FROM customer;
+
+-- SELECT DISTINCT release_year FROM film;
+-- SELECT DISTINCT rental_rate FROM film; 
+
+
+
+
+
+
+
