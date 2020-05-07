674eb195b69371cf0d0e18ff31d7dc2abe74b3e0
Mahnoor Anjum <manomaq@gmail.com>
2020-05-07 07:21:45 +0500

    updated repository

---
+++ s5_orderby.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM payment; 
+
+-- SELECT customer_id, amount 
+-- FROM payment 
+-- ORDER BY amount DESC, customer_id ASC;
+
+-- SELECT DISTINCT amount 
+-- FROM payment 
+-- ORDER BY amount DESC
+-- LIMIT 10;
