b889010b7fb9af853e35634db9be5e595cfc467e
Mahnoor Anjum <manomaq@gmail.com>
2020-05-08 05:27:34 +0500

    updated sql files

---
+++ s19_operators.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM payment
+
+-- SELECT staff_id + amount AS staff_amount
+-- FROM payment 
+
+-- SELECT staff_id - amount AS staff_amount
+-- FROM payment 
+
+-- SELECT amount / staff_id  AS staff_amount
+-- FROM payment 
