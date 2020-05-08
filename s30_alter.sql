5ea8999b2905270ca1e5064bbf9e84fa60806871
Mahnoor Anjum <manomaq@gmail.com>
2020-05-09 00:39:19 +0500

    updated sql files

---
+++ s30_alter.sql
@@ -0,0 +1,23 @@
+-- DROP TABLE IF EXISTS alterio
+
+-- CREATE TABLE alterio(
+-- 	ids serial PRIMARY KEY, 
+-- 	title VARCHAR(200) NOT NULL,
+-- 	rating VARCHAR(10) NOT NULL)
+
+-- SELECT * FROM alterio
+
+-- ALTER TABLE alterio ADD COLUMN appendo BOOLEAN
+
+-- SELECT * FROM alterio
+
+-- ALTER TABLE alterio DROP COLUMN appendo 
+
+-- SELECT * FROM alterio
+
+-- ALTER TABLE alterio RENAME COLUMN ids TO sno 
+
+SELECT * FROM alterio
+
+
+
