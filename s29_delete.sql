9a9083e3ff4a516c9b8b65dca8996aa0aaac167f
Mahnoor Anjum <manomaq@gmail.com>
2020-05-09 00:38:24 +0500

    updated sql files

---
+++ s29_delete.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM account
+
+-- DELETE FROM account
+-- WHERE username NOT IN ('A', 'B', 'C')
+
+-- DELETE FROM account
+-- WHERE username NOT IN ('A', 'B', 'C')
+-- RETURNING username
+
+SELECT * FROM account
