b9110716453c3d1fd72bebde96fb3a19d9b6fb40
Mahnoor Anjum <manomaq@gmail.com>
2020-05-08 05:29:15 +0500

    updated sql files

---
+++ s26_insert.sql
@@ -0,0 +1,6 @@
+-- SELECT * FROM account
+
+-- INSERT INTO account(username, password, email, created)
+-- VALUES ('mahnoor', 'password', 'mahnoor@gmail.com', CURRENT_TIMESTAMP)
+
+SELECT * FROM account
